3c6b1b220f8400e95937252eec0d67c531d42c21
iFergal <fergaloconnor1998@gmail.com>
2023-10-30 20:52:56 -0500

    docs: README updates

--- verification-scripts/src/cid-verify.ts
+++ verification-scripts/src/cid-verify.ts
@@ -3,6 +3,7 @@ import { code } from "multiformats/codecs/raw";
 import { from } from "multiformats/hashes/hasher";
 import { base58btc } from "multiformats/bases/base58";
 import { createHash } from "blake2";
+import canonicalize from "canonicalize";
 
 const blake2b256 = from({
   name: 'blake2b-256',
@@ -18,9 +19,14 @@ async function run() {
   if (process.argv.length !== 3) {
     throw new Error("You must provide the off-chain data as an argument without spaces");
   }
-  
+
   // JSON.parse since we are wrapping in single quotes
-  const data = Buffer.from(JSON.stringify(JSON.parse(process.argv[2]))); 
+  const canonicalized = canonicalize(JSON.parse(process.argv[2]));
+  if (!canonicalized) {
+    throw new Error("Error canonicalising data...");
+  }
+  
+  const data = Buffer.from(canonicalized);
   const multihash = await blake2b256.digest(data);
   console.log(`CID is: ${CID.createV1(code, multihash).toString(base58btc.encoder)}`);
 }
